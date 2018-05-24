06d845ae710c344a42ddef3080267770359cfb29
Matti Bijnens <matti.bijnens@student.howest.be>
2018-05-24 11:03:12 +0200

    Initial Commit

---
+++ LightDemo/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightDemo
+{
+    class Light
+    {
+        public int LightId { get; set; }
+        public Light(int id)
+        {
+            this.LightId = id;
+        }
+    }
+
+    class LightAndState
+    {
+        public Light Light { get; set; }
+        public LightState State { get; set; }
+
+        public LightAndState(Light light, LightState state)
+        {
+            this.Light = light;
+            this.State = state;
+        }
+    }
+
+    enum LightState
+    {
+        GREEN, AMBER, RED
+    }
+}
--- LightDemo/Program.cs
+++ LightDemo/Program.cs
@@ -4,9 +4,34 @@ namespace LightDemo
 {
     class Program
     {
+        LightAndState[] state1 = { new LightAndState(new Light(1), LightState.GREEN), new LightAndState(new Light(2), LightState.RED) };
+        LightAndState[] state2 = { new LightAndState(new Light(1), LightState.AMBER), new LightAndState(new Light(2), LightState.RED) };
+        LightAndState[] state3 = { new LightAndState(new Light(1), LightState.RED), new LightAndState(new Light(2), LightState.GREEN) };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().Run();   
+        }
+        public void Run()
+        {
+            Console.WriteLine("State 1 -----");
+            DoChanges(state1);
+            Console.WriteLine("-------------\n\n");
+            Console.WriteLine("State 2 -----");
+            DoChanges(state2);
+            Console.WriteLine("-------------\n\n");
+            Console.WriteLine("State 3 -----");
+            DoChanges(state3);
+            Console.WriteLine("-------------\n\n");
+
+            Console.Read();
+        }
+
+        public void DoChanges(LightAndState[] states)
+        {
+            foreach (LightAndState lightAndState in states)
+            {
+                Console.WriteLine("Changing light " + lightAndState.Light.LightId + " to " + lightAndState.State);
+            }
         }
     }
 }
